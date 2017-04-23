a28430c4a029b85c03ecd352c1121bf5ed4bc8a8
Evin Callahan <gaieges@gmail.com>
2017-04-23 11:42:47 -0500

    testing js-ipfs

---
+++ test/get-and-put-object.js
@@ -0,0 +1,28 @@
+var IPFS = require('ipfs');
+
+var ipfs = new IPFS({start: true, init: true})
+
+var obj = {
+  Data: new Buffer('awesome data here 123556'), 
+  Links:[]
+};
+
+
+var meh = ipfs.on('ready', () => {
+  ipfs.object.put(obj,(putErr,putNode) => { 
+    if( putErr )
+      return console.error(putErr)
+
+    var hash = putNode.toJSON().multihash
+
+    console.log("successfully put: %s", hash );
+
+    ipfs.object.get(hash,(gotErr, gotNode) => {
+      if( gotErr )
+        return console.error(gotErr)
+
+      console.log( "successfully got: %s", gotNode.toJSON() ); 
+    })
+  })
+
+})
